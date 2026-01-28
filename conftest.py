d1ac37739456cdf07f87d0def58681b210469ece
Viktor Lata <vikost@yandex.ru>
2026-01-28 15:31:58 +0500

    remove comments in conftest.py

--- conftest.py
+++ conftest.py
@@ -1,13 +1,9 @@
 import pytest
 from selenium import webdriver
 from locators import PageLocators
-#from selenium.webdriver.chrome.service import Service
-#from webdriver_manager.chrome import ChromeDriverManager
 
 @pytest.fixture
 def driver():
-    #service = Service(ChromeDriverManager().install())
-    #driver = webdriver.Chrome(service=service)
     driver = webdriver.Chrome()
     driver.maximize_window()
     driver.get(PageLocators.SERVICE_URL)
